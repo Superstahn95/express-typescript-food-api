faaad22961bc4c6329906e04a702641565d0b77e
superstahn <superstahn@gmail.com>
2024-03-13 15:21:30 +0100

    added a utitily function for deleting files from the public folder

---
+++ src/utils/deleteFile.ts
@@ -0,0 +1,41 @@
+import { unlink, stat, PathLike, Stats } from "fs";
+
+export const deleteFile = (filePath: PathLike) => {
+  stat(filePath, (err: NodeJS.ErrnoException | null, stats: Stats) => {
+    if (
+      process.env.NODE_ENVIRONMENT &&
+      process.env.NODE_ENVIRONMENT === "development"
+    ) {
+      console.log(stats);
+    }
+    //if we get an error
+    if (
+      err &&
+      process.env.NODE_ENVIRONMENT &&
+      process.env.NODE_ENVIRONMENT === "development"
+    ) {
+      console.log("failed to find file... check below error message");
+      console.log(err);
+    } else {
+      //once there is no error and file path is found, delete file using unlink method in the fs module
+      unlink(filePath, (err: NodeJS.ErrnoException | null) => {
+        if (
+          err &&
+          process.env.NODE_ENVIRONMENT &&
+          process.env.NODE_ENVIRONMENT === "development"
+        ) {
+          console.log("Something went wrong and file couldn't be deleted");
+          return console.log(err);
+        }
+        if (
+          process.env.NODE_ENVIRONMENT &&
+          process.env.NODE_ENVIRONMENT === "development"
+        ) {
+          console.log("File deleted successfully");
+        }
+      });
+    }
+  });
+};
+
+// stat(path: PathLike, callback: (err: NodeJS.ErrnoException | null, stats: Stats) => void): void
